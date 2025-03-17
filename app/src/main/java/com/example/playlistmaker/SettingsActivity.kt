a065efd13694af0db5b130b131a266035386b12e
IgorPonomarenko01 <ipon1201@gmail.com>
2025-03-17 18:02:05 +0300

    3-й коммит. Добавил всё в стили. Padding справа и слева сделал, как в figma. Добавил ссылки на цвета в svg

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,14 +1,19 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings2)
+
+        val navBack = findViewById<MaterialToolbar>(R.id.tool_bar)
+        navBack.setNavigationOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
         }
     }
