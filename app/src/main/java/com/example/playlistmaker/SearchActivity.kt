57039147f1730a03129088321abbdd50e79eb229
IgorPonomarenko01 <ipon1201@gmail.com>
2025-03-30 11:42:05 +0300

    Скорректирован экран поиск

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val navBack = findViewById<MaterialToolbar>(R.id.tool_bar)
+
+        navBack.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
