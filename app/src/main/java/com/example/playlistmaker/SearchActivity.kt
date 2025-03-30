4ddff6186d074fa22ba6f16fd9602f339f5c7eab
IgorPonomarenko01 <ipon1201@gmail.com>
2025-03-30 14:29:11 +0300

    Реализован функционал строки поиска

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,14 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
@@ -16,6 +20,37 @@ class SearchActivity : AppCompatActivity() {
 
         navBack.setNavigationOnClickListener {
             finish()
+        }
+        val searchInput = findViewById<EditText>(R.id.searchInput)
+        val searchInputClear = findViewById<ImageView>(R.id.searchInputClear)
+
+        searchInputClear.setOnClickListener {
+            searchInput.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchInput.windowToken, 0)
+        }
+
+        val searchInputTextWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchInputClear.visibility = inputClearVisibility(s)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        }
+
+        searchInput.addTextChangedListener(searchInputTextWatcher)
+
+    }
+
+    private fun inputClearVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
